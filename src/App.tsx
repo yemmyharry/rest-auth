e402e88a67e3a09f5877fbc6425756f30c2bc690
yemmyharry <yemmyharry@gmail.com>
2022-05-11 21:52:51 +0100

    Add home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import SignIn from "./components/SignIn"
+import{ Header} from "./components/SignUp"
+import Home from "./components/Home"
 
-function App() {
+import { BrowserRouter, Routes, Route} from "react-router-dom"
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <h1 className="text-3xl font-bold underline">
+    
 
-export default App;
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<Header />} />
+        </Routes>
+      </BrowserRouter>
+    </h1>
+  )
+}
