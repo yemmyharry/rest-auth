049668fbd962fb5fab92898bb29945fb562f5409
yemmyharry <yemmyharry@gmail.com>
2022-05-11 23:54:45 +0100

    refactored code

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,16 @@
-import SignIn from "./components/SignIn"
-import{ Header} from "./components/SignUp"
-import Home from "./components/Home"
-
 import { BrowserRouter, Routes, Route} from "react-router-dom"
+import SignIn from "./components/SignIn"
+import SignUp from "./components/SignUp"
+import Home from "./components/Home"
 
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-    
-
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/signin" element={<SignIn />} />
-          <Route path="/signup" element={<Header />} />
+          <Route path="/signup" element={<SignUp />} />
         </Routes>
       </BrowserRouter>
-    </h1>
   )
 }
